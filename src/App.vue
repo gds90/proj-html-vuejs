5d3563feb97335392e62515c62127fa64a4f1299
gds90 <you@example.com>
2024-01-17 17:06:47 +0100

    Added components and generals css

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,6 @@ export default {
     
   </div>
 </template>
-<style lang="">
-  
+<style lang="scss">
+@use './assets/styles/generals.scss'
 </style>
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    blogCards: [
+        {
+            image: '',
+            time: '10 min',
+            views: '240',
+            date: 'Apr 11, 2022',
+            titles: 'Secrets To An Amazing Role Playing Game'
+        }
+    ]
+})
