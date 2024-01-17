efab4b3538ea4d64735acc97905cc53f4f7c12ef
pietrostraneo <straneo_p@yahoo.it>
2024-01-17 18:13:08 +0100

    Created feedbackCard component

---
+++ src/components/FeedbackCard.vue
@@ -0,0 +1,90 @@
+<script>
+export default {
+    name: 'FeedbackCard',
+    data() {
+        return {
+            reviews: [
+                {
+                    profileimg: '/public/img/testimonials-image-1.png',
+                    name: 'Kim Ramos',
+                    location: 'Serbia'
+                },
+                {
+                    profileimg: '/public/img/testimonials-image-2.png',
+                    name: 'Garry Francis',
+                    location: 'Kiribati'
+                },
+                {
+                    profileimg: '/public/img/testimonials-image-3.png',
+                    name: 'Merle Fisher',
+                    location: 'United States'
+                },
+            ]
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<template lang="">
+    <div class="col-12 col-md-4 feedback-cards" v-for="rate, index in reviews" :key="index">
+        <div class="stars d-flex gap-1 mb-2">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+        </div>
+        <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.”</p>
+        <div class="user d-flex gap-3 align-content-center">
+            <div class="user-img d-flex align-items-center">
+                <img :src="rate.profileimg" :alt="rate.name" class="img-fluid">
+            </div>
+            <div class="user-data">
+                <h6>{{ rate.name }}</h6>
+                <p> {{ rate.location }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../assets/styles/generals.scss';
+@use '../assets/styles/partials/variables' as *;
+
+.fa-star {
+    color: gold;
+    font-size: 18px;
+}
+
+.feedback-cards {
+    border-radius: 30px;
+    background-color: $colorblue;
+    padding: 34px 40px;
+    margin: 0 10px;
+    width: 432px;
+
+    p,
+    h6 {
+        color: white;
+    }
+
+    p {
+        font-size: 25px;
+    }
+
+    h6 {
+        font-size: 25px;
+    }
+
+    img {
+        width: 40px;
+        height: 40px;
+    }
+
+    .user-data p {
+        color: $colorlime;
+        font-size: 18px;
+    }
+}
+</style>
