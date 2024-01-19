5fa4c58892f2410c4b501ba9f08b2e76505317db
pietrostraneo <straneo_p@yahoo.it>
2024-01-19 16:34:55 +0100

    Added feedback-animations

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,43 +13,43 @@ export default {
                     section: 'Game With Live Stearming',
                     title: 'Future Of Esports',
                     subtitle: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: 'The Ultimate Competitive Gaming Platfrom',
                     title: 'Make Your Mark On The Battle Field',
                     subtitle: 'libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: 'Trending Battle',
                     title: 'Keep Eyes & Manage Upcoming Battle',
                     subtitle: 'libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: '',
                     title: 'Futio Quick Facts',
                     subtitle: '',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: 'Live Streaming',
                     title: 'Watch The Streams',
                     subtitle: 'libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: 'Futio Feedback',
                     title: 'What Our Gamers Say',
                     subtitle: 'libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Cras ultricies ligula sed magna dictum porta.',
-                    isWhite: false 
+                    isWhite: false
                 },
                 {
                     section: 'Make Offer',
                     title: 'Get 70% Offer Discount',
                     subtitle: '',
-                    isWhite: true 
+                    isWhite: true
                 },
                 {
                     section: 'Latest News',
@@ -311,6 +311,8 @@ export default {
                         <div class="feedback-header text-center">
                             <Title :titles="titlesSections[5]" />
                         </div>
+                    </div>
+                    <div class="col-12">
                         <div class="feedback-card d-flex justify-content-center">
                             <FeedbackCard />
                         </div>
@@ -474,13 +476,16 @@ export default {
             background-color: $colorgreen;
             color: $colorblack;
             @include btn-lime;
+
             &:hover {
-            background-color: $colorlime;
-            }   
+                background-color: $colorlime;
+            }
         }
+
         .border-btn {
             color: $colorblack;
             @include btn-border-lime;
+
             &::after {
                 position: absolute;
                 content: "";
@@ -514,13 +519,16 @@ export default {
             background-color: $colorgreen;
             color: $colorblack;
             @include btn-lime;
+
             &:hover {
-            background-color: $colorlime;
-            }   
+                background-color: $colorlime;
+            }
         }
+
         .border-btn {
             color: $colorblack;
             @include btn-border-lime;
+
             &::after {
                 position: absolute;
                 content: "";
@@ -646,5 +654,4 @@ export default {
     .feedback-header {
         margin-bottom: 55px;
     }
-}
-</style>
+}</style>
